dd44eb53e326c87cb8ea22b6be0616e2b25929c5
Benoit Mrejen <benoit.mrejen@eridanis.com>
2021-11-30 02:53:08 +0100

    initial migration ok

--- contracts/StakeCoin.sol
+++ contracts/StakeCoin.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StakeCoin is Ownable, ERC20 {
-  constructor(string memory name, string memory symbol) public ERC20("StakeCoin", "SKC") public {
+  constructor() ERC20("StakeCoin", "SKC") {
     _mint(msg.sender, 100 * 10 * uint(decimals()));
   }
 }
